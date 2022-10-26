046e7e9c76fbd08de0916c8b0de5297637206d25
Adon Metcalfe <adonm@fastmail.fm>
2022-10-26 13:44:11 +0800

    Handle running in a non interactive session

--- siem_query_utils/__init__.py
+++ siem_query_utils/__init__.py
@@ -25,8 +25,8 @@ def index():
 
 
 def serve():
-    # launch background node helper on port 3000
-    atlaskit = Popen("node atlaskit-transformer/main.mjs", shell=True, close_fds=True)
+    # launch background node helper on port 3000 (handle running in a non interactive session for nvm/node access)
+    atlaskit = Popen("bash -l -c 'node atlaskit-transformer/main.mjs'", shell=True, close_fds=True)
     # serve on port 8000, assume running behind a trusted reverse proxy
     host, port = "0.0.0.0", 8000
     # Launch main uvicor server
