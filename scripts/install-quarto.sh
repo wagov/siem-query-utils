20b10d3a067cf79807093c17ba8ea4bbbea6cb07
Adon Metcalfe <adonm@fastmail.fm>
2023-02-01 23:15:22 +0000

    quarto enhancements

--- scripts/install-quarto.sh
+++ scripts/install-quarto.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ $(uname -m) == "x86_64" ]]; then # grab prebuilt version
     curl -sL https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.142/quarto-1.3.142-linux-amd64.deb -o /tmp/quarto.deb
-    apt-get -y install /tmp/quarto.deb && rm /tmp/quarto.deb;
+    apt-get -y install /tmp/quarto.deb && rm /tmp/quarto.deb
+    quarto install tool tinytex
 else
     echo "sorry no quarto for arm64 yet"
 fi
